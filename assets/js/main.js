3aa22aff391850d296259396c9f882481f8ba783
franklin-raph <igboekwulusifranklin@gmail.com>
2023-06-28 23:33:30 +0100

    completed the desktop view

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,19 @@ document.querySelectorAll(".faq").forEach(faq => {
         faq.classList.toggle('shadow')
     })
 })
+
+const backtotop = document.querySelector(".backtotop")
+
+window.addEventListener("scroll", function() {
+    //when the user scrolls 30px from the top, show the 'back-to-top' button(first for crome, second for Safari)
+    if(document.documentElement.scrollTop >= 30 || document.body.scrollTop >= 30) {
+        backtotop.style.bottom = "20px"
+    } else{
+        backtotop.style.bottom = "-50px"
+    }
+})
+
+backtotop.addEventListener("click", () => {
+    document.body.scrollTop = "0"
+    document.documentElement.scrollTop = 0;
+})
