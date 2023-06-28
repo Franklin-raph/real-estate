8840eb83826e6916d8f27967f139a0f534448814
franklin-raph <igboekwulusifranklin@gmail.com>
2023-06-29 00:06:18 +0100

    updated the UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,33 @@ document.querySelectorAll(".faq").forEach(faq => {
     })
 })
 
-const backtotop = document.querySelector(".backtotop")
+const backtotop = document.querySelector(".backtotop");
+const nav = document.querySelector("nav")
 
 window.addEventListener("scroll", function() {
     //when the user scrolls 30px from the top, show the 'back-to-top' button(first for crome, second for Safari)
-    if(document.documentElement.scrollTop >= 30 || document.body.scrollTop >= 30) {
+    if(document.documentElement.scrollTop >= 550 || document.body.scrollTop >= 550) {
         backtotop.style.bottom = "20px"
+        nav.style.top = "-100px"
     } else{
         backtotop.style.bottom = "-50px"
+        nav.style.top = "0"
     }
 })
 
 backtotop.addEventListener("click", () => {
     document.body.scrollTop = "0"
     document.documentElement.scrollTop = 0;
+})
+
+window.addEventListener("scroll", () => {
+    if(document.documentElement.scrollTop >= 30 || document.body.scrollTop >= 30) {
+        nav.style.backgroundColor = "#302E2E"
+        nav.style.position = "fixed"
+        
+    } else{
+        nav.style.position = "relative"
+        nav.style.backgroundColor = "transparent"
+        
+    }
 })
