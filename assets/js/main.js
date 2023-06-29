fa1dff5101ad598668ad646f3af63e75caa00903
franklin-raph <igboekwulusifranklin@gmail.com>
2023-06-29 03:18:52 +0100

    added animation library

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,30 @@
-$('.owl-carousel').owlCarousel({
-    items: 3,
-    loop: true,
-    autoplay: true,
-    margin: 50,
-    dots:true,
-    arrows:true,
-    nav: false,
-    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-    responsive:{
-        0:{
-            items:1,
-            nav:true,
-            loop:true
-        },
-        700:{
-            items:2,
-            nav:true,
-            loop:true
-        },
-        1000:{
-            items:4,
-            nav:true,
-            loop:true
-        }
-    }
-})
-
+$(".owl-carousel").owlCarousel({
+  items: 3,
+  loop: true,
+  autoplay: true,
+  margin: 50,
+  dots: true,
+  arrows: true,
+  nav: false,
+  navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+      loop: true,
+    },
+    700: {
+      items: 2,
+      nav: true,
+      loop: true,
+    },
+    1000: {
+      items: 4,
+      nav: true,
+      loop: true,
+    },
+  },
+});
 
 document.querySelectorAll(".faq").forEach((faq) => {
   faq.addEventListener("click", function () {
@@ -62,3 +61,72 @@ window.addEventListener("scroll", () => {
     nav.style.backgroundColor = "transparent";
   }
 });
+
+function openLoginModal() {
+  document.querySelector(".login-modal-bg").style.display = "flex";
+}
+
+function closeLoginModal() {
+  document.querySelector(".login-modal-bg").style.display = "none";
+}
+
+function openRegisterModal() {
+  document.querySelector(".register-modal-bg").style.display = "flex";
+}
+
+function closeRegisterModal() {
+  document.querySelector(".register-modal-bg").style.display = "none";
+}
+
+document.querySelector("#closeLoginForm").addEventListener("click", () => {
+  closeLoginModal();
+});
+
+document.querySelector("nav li:last-child").addEventListener("click", () => {
+  openLoginModal();
+});
+
+document.querySelector("#getStarted button").addEventListener("click", () => {
+  openLoginModal();
+});
+
+document.querySelector(".registerButton").addEventListener("click", () => {
+  closeLoginModal();
+  openRegisterModal();
+});
+
+document.querySelector(".LoginButton").addEventListener("click", () => {
+  closeRegisterModal();
+  openLoginModal();
+});
+
+document.querySelector("#closeRegForm").addEventListener("click", () => {
+  closeRegisterModal();
+});
+
+document.querySelector(".waitlist button").addEventListener("click", (e) => {
+  const email = document.querySelector(".waitlist input[type=text]");
+  console.log(email);
+  e.preventDefault();
+  if (email.value <= 0) {
+    email.placeholder = "Please enter an email address";
+    email.classList.add("error");
+    document.querySelector(".form input").style.border = "2px solid #970000";
+  } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+    document.querySelector("small").style.display = "block";
+    email.classList.add("error");
+    document.querySelector(".form input").style.border = "2px solid #970000";
+  } else {
+    document.querySelector("small").style.display = "none";
+    email.classList.remove("error");
+    email.placeholder = "jhondoe@gmail.com";
+    document.querySelector(".form input").style.border = "2px solid green";
+  }
+});
+
+$(".counter").counterUp({
+  delay: 20,
+  time: 2000,
+});
+
+AOS.init();
